22b04d8ee8e55e7c7d500d065973322ba33a1290
Andrey-v42 <andrey.henrique28@gmail.com>
2025-01-24 10:36:17 -0300

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/plano-digital/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: '_redirects', dest: '' } // Ensure the _redirects file is copied to the root of dist
+      ]
+    })
+  ],
 })
