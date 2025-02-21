c0663d6df144f88a7dd1d35e4c0bd331496b4288
Andrey-v42 <andrey.henrique28@gmail.com>
2025-02-21 14:56:38 -0300

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/plano-operacional/',
   plugins: [
     react(),
     viteStaticCopy({
